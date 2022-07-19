2045e8c281e6066c7b443247d3220417beecf2cf
chping <chping@iowk.com>
2022-07-19 14:54:01 +0800

    string字符串的操作

--- matching/src/lib.rs
+++ matching/src/lib.rs
@@ -11,6 +11,20 @@ pub fn match_fn(){
     if let Some(f) = x  {
         println!("模式匹配结果值{}",f)
     }
+    //匹配对应类型
+    let dog = Animal::Dog;
+    match  dog {
+        Animal::Monkey =>println!("孙悟空"),
+        Animal::Dog =>  println!("哮天犬"),
+        Animal::Pig => println!("小猪佩奇"),
+        Animal::Chicken => println!("卯天星君"),
+    }
 }
 //模式匹配 结果上的匹配 解构
 
+pub enum Animal {
+    Monkey,
+    Dog,
+    Pig,
+    Chicken
+}
--- strings/src/lib.rs
+++ strings/src/lib.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,4 +7,28 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn new_string_test(){
+        crate::new_string();
+    }
+}
+
+
+fn new_string(){
+    //创建一个空字符串
+    let str1 = String::new();
+    //字符切片中创建一个String类型
+    let str2  = String::from("你好呀");
+    let bytes :&[u8] = b"\xE4\xB8\x96\xE7\x95\x8C";
+    //byte数组转换为字符串
+    let str3 =String::from_utf8(bytes.to_vec());
+    println!("{:?}",str3);
+
+    let str4 = String::from("Hello world");
+    //将字符转为char数组
+    let chars:Vec<char> = str4.chars().rev().collect();
+    println!("{:?}",chars);
+    
+
 }
